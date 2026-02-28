420e1dae1976e4c8eac9b82539343419f9e5c8c6
Vikramsindra <vikramsindra931@gmail.com>
2026-02-28 15:07:43 +0530

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import matchRouter from "./routes/matches.js";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("server is working")
 })
+
+app.use("/matches", matchRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
---
+++ src/routes/matches.js
@@ -0,0 +1,64 @@
+import { Router } from "express";
+import {
+    createMatchSchema,
+    listMatchesQuerySchema,
+} from "../validation/matches.js";
+import { db } from "../db/db.js";
+import { matches } from "../db/schema.js";
+import { getMatchStatus } from "../utils/match_status.js";
+import { desc } from "drizzle-orm";
+const matchRouter = Router();
+
+const MAX_LIMIT = 100;
+
+matchRouter.get("/", async (req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.query);
+    if (!parsed.success) {
+        return req.status(400).json({ error: "Invalid Query" });
+    }
+    const limit = Math.min(parsed.data.limit ?? 0, MAX_LIMIT);
+
+    try {
+        const data = await db
+            .select()
+            .from(matches)
+            .orderBy(desc(matches.createdAt));
+
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to list Matches" });
+    }
+});
+
+matchRouter.post("/", async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+    if (!parsed.success) {
+        return res.status(400).json({
+            error: "Invalid payload",
+            details: JSON.stringify(parsed.error),
+        });
+    }
+    const { startTime, endTime, homeScore, awayScore } = parsed.data;
+
+    try {
+        const [event] = await db
+            .insert(matches)
+            .values({
+                ...parsed.data,
+                startTime: new Date(parsed.data.startTime),
+                endTime: new Date(parsed.data.endTime),
+                homeScore: homeScore ?? 0,
+                awayScore: awayScore ?? 0,
+                status: getMatchStatus(startTime, endTime),
+            })
+            .returning();
+
+        res.status(201).json({ data: event });
+    } catch (e) {
+        res
+            .status(500)
+            .json({ error: "Failed to create match", details: JSON.stringify(e) });
+    }
+});
+
+export default matchRouter;
